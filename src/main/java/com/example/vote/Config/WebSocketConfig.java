7ce29380201d255f35dbbb67373fc3363ed2eae6
dbspgnl <dbspgnl@naver.com>
2021-10-08 23:56:30 +0900

    first commit

---
+++ src/main/java/com/example/vote/Config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.example.vote.Config;
+
+import com.example.vote.Service.SocketTextHandler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+	@Autowired
+	SocketTextHandler socketTextHandler;
+	
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(socketTextHandler, "/chat");
+	}
+
+}
