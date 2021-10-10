ebc6769289cb0158244127976210e17b17320f7e
dbspgnl <dbspgnl@naver.com>
2021-10-11 00:58:03 +0900

    반환값 json으로 처리

---
+++ src/main/java/com/example/vote/Model/Information.java
@@ -0,0 +1,11 @@
+package com.example.vote.Model;
+
+import lombok.Data;
+
+@Data
+public class Information {
+
+    int count;
+    String payload;
+    
+}
--- src/main/java/com/example/vote/Service/SocketTextHandler.java
+++ src/main/java/com/example/vote/Service/SocketTextHandler.java
@@ -2,6 +2,10 @@ package com.example.vote.Service;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
+
+import com.example.vote.Model.Information;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
@@ -9,10 +13,15 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import lombok.RequiredArgsConstructor;
+
 @Component
+@RequiredArgsConstructor 
 public class SocketTextHandler extends TextWebSocketHandler {
 
-//	ArrayList<WebSocketSession> sessions = new ArrayList<>();
+	private Integer count = 0;
+
+	//ArrayList<WebSocketSession> sessions = new ArrayList<>();
 	HashMap<String, WebSocketSession> sessions = new HashMap<>();
 
 	// client에서 메시지가 서버로 전송댈때 실행되는 함수.
@@ -22,11 +31,26 @@ public class SocketTextHandler extends TextWebSocketHandler {
 
 		try {
 
-				// 접속된 모든 세션에 메시지 전송
-				for (String key : sessions.keySet()) {
-					WebSocketSession ss = sessions.get(key);
-					ss.sendMessage(new TextMessage(payload));
-				}
+			this.count += 1;
+			String html = "<h1>수:</h1>";
+			Map<String, String> map = new HashMap<>();
+
+			map.put("ctn", count.toString());
+			map.put("payload", payload);
+
+			Information info = new Information();
+
+			info.setCount(this.count);
+			info.setPayload(payload);
+
+			ObjectMapper objectMapper = new ObjectMapper();
+
+			// 접속된 모든 세션에 메시지 전송
+			for (String key : sessions.keySet()) {
+				WebSocketSession ss = sessions.get(key);
+				// ss.sendMessage(new TextMessage(payload));
+				ss.sendMessage(new TextMessage(objectMapper.writeValueAsString(info)));
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
