5b8924ebe10e9b612648924edf05c0de7f7174d2
dbspgnl <dbspgnl@naver.com>
2021-10-17 00:03:40 +0900

    채팅방과 채팅 url 중복 문제 해결

--- src/main/java/com/example/vote/Config/WebSocketConfig.java
+++ src/main/java/com/example/vote/Config/WebSocketConfig.java
@@ -16,7 +16,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 	SocketTextHandler socketTextHandler;
 	
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(socketTextHandler, "/chat");
+		registry.addHandler(socketTextHandler, "/chat/message");
 	}
 
 }
