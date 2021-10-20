2bc471b95b6525b6345166916e9b66003fb8394e
dbspgnl <dbspgnl@naver.com>
2021-10-21 00:15:50 +0900

    원정대 출정 기능 구현(테스트 더 필요)

--- src/main/java/com/example/vote/Service/SocketAvalonService.java
+++ src/main/java/com/example/vote/Service/SocketAvalonService.java
@@ -24,6 +24,11 @@ public class SocketAvalonService extends TextWebSocketHandler {
 	private Integer agreeCtn = 0;
 	private Integer disagreeCtn = 0;
 	
+	private Integer campaignerCtn = 0;
+	private Integer voteCampaignCtn = 0;
+	private Integer successCtn = 0;
+	private Integer failCtn = 0;
+	
 	HashMap<String, WebSocketSession> sessions = new HashMap<>();
 
 	// client에서 메시지가 서버로 전송댈때 실행되는 함수.
@@ -31,7 +36,6 @@ public class SocketAvalonService extends TextWebSocketHandler {
 	public void handleTextMessage(WebSocketSession session, TextMessage message) {
 		String payload = message.getPayload();
 		Map<String, String> mapData = new HashMap<>();
-		// System.out.println(payload);
 		try {	
 
 			//초기화
@@ -39,6 +43,14 @@ public class SocketAvalonService extends TextWebSocketHandler {
 				mapData.put("message", "Reset");
 				mapData.put("agree", "0"); 
 				mapData.put("disagree", "0"); 
+				sendMessage(mapData);
+			}
+			if(payload.equals("#resetCampaign")){
+				this.campaignerCtn = 0;
+				this.voteCampaignCtn = 0;
+				mapData.put("message", "ResetCampaign");
+				mapData.put("success", "0"); 
+				mapData.put("fail", "0"); 
 				sendMessage(mapData);
 			}
 
@@ -55,6 +67,23 @@ public class SocketAvalonService extends TextWebSocketHandler {
 						this.disagreeCtn += 1;
 						this.voteCtn += 1;
 						mapData.put("message", "voting"); 
+						sendMessage(mapData);
+						break;
+					case "#toVoteSuccess":
+						this.successCtn += 1;
+						this.voteCampaignCtn += 1;
+						mapData.put("message", "comping"); 
+						sendMessage(mapData);
+						break;
+					case "#toVoteFail":
+						this.failCtn += 1;
+						this.voteCampaignCtn += 1;
+						mapData.put("message", "comping"); 
+						sendMessage(mapData);
+						break;
+					case "#tap2PlayerPlus":
+						this.campaignerCtn += 1;
+						mapData.put("campaignerCtn", this.campaignerCtn.toString()); 
 						sendMessage(mapData);
 						break;
 					default:
@@ -83,6 +112,40 @@ public class SocketAvalonService extends TextWebSocketHandler {
 					sendMessage(mapData);
 				}
 			}
+			if(this.campaignerCtn == this.voteCampaignCtn && this.voteCampaignCtn != 0){ 
+				if(this.failCtn == 0){
+					mapData.put("message", "Campaign_Success"); 
+					mapData.put("success", this.successCtn.toString()); 
+					mapData.put("fail", this.failCtn.toString()); 
+					this.voteCampaignCtn = 0;
+					this.successCtn = 0;
+					this.failCtn = 0;
+					this.campaignerCtn = 0;
+					sendMessage(mapData);
+				}
+				else{
+					mapData.put("message", "Campaign_Failure");
+					mapData.put("success", this.successCtn.toString()); 
+					mapData.put("fail", this.failCtn.toString()); 
+					this.voteCampaignCtn = 0;
+					this.successCtn = 0;
+					this.failCtn = 0;
+					this.campaignerCtn = 0;
+					sendMessage(mapData);
+				}
+			}
+
+			// System.out.println("payload:"+payload);
+			// System.out.println("totalPlayerCtn:"+totalPlayerCtn);
+			// System.out.println("voteCtn:"+voteCtn);
+			// System.out.println("agreeCtn:"+agreeCtn);
+			// System.out.println("disagreeCtn:"+disagreeCtn);
+			// System.out.println("");
+			// System.out.println("campaignerCtn:"+campaignerCtn);
+			// System.out.println("voteCampaignCtn:"+voteCampaignCtn);
+			// System.out.println("successCtn:"+successCtn);
+			// System.out.println("failCtn:"+failCtn);
+			// System.out.println("---------------------");
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -95,6 +158,10 @@ public class SocketAvalonService extends TextWebSocketHandler {
 		super.afterConnectionEstablished(session);
 		// System.out.println("session.getId():"+session.getId());
 		this.totalPlayerCtn ++;
+		this.voteCampaignCtn = 0;
+		this.successCtn = 0;
+		this.failCtn = 0;
+		this.campaignerCtn = 0;
 		sessions.put(session.getId(), session);
 		Map<String, String> mapData = new HashMap<>();
 		mapData.put("totalPlayerCtn", this.totalPlayerCtn.toString());
@@ -106,6 +173,10 @@ public class SocketAvalonService extends TextWebSocketHandler {
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 		sessions.remove(session.getId());
 		this.totalPlayerCtn --;
+		this.voteCampaignCtn = 0;
+		this.successCtn = 0;
+		this.failCtn = 0;
+		this.campaignerCtn = 0;
 		super.afterConnectionClosed(session, status);
 		Map<String, String> mapData = new HashMap<>();
 		mapData.put("totalPlayerCtn", this.totalPlayerCtn.toString());
