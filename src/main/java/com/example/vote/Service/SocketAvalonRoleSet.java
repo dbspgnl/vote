6715f03c98f9e3f1f7bbc6cc38929cef54d06c19
dbspgnl <dbspgnl@naver.com>
2021-10-24 21:44:51 +0900

    역할 배정 기능 구현

--- src/main/java/com/example/vote/Service/SocketAvalonRoleSet.java
+++ src/main/java/com/example/vote/Service/SocketAvalonRoleSet.java
@@ -21,7 +21,13 @@ public class SocketAvalonRoleSet extends TextWebSocketHandler {
 
 	private Integer totalPlayerCtn = 0;
 
+    private String assasin;
     private String merlin;
+    private String minion;
+    private String morcana;
+    private String mordred;
+    private String oberon;
+    private String percival;
 	
 	HashMap<String, WebSocketSession> sessions = new HashMap<>();
 
@@ -30,12 +36,74 @@ public class SocketAvalonRoleSet extends TextWebSocketHandler {
 	public void handleTextMessage(WebSocketSession session, TextMessage message) {
 		String payload = message.getPayload();
 		Map<String, String> mapData = new HashMap<>();
+		// List<String> list = new ArrayList<>();
 		try {	
             
             System.out.println(payload);
 
-			mapData.put("totalPlayerCtn", this.totalPlayerCtn.toString());
-		    sendMessage(mapData);
+			if(payload.equals("#reset")){
+				this.assasin = "";
+				this.merlin = "";
+				this.minion = "";
+				this.morcana = "";
+				this.mordred = "";
+				this.oberon = "";
+				this.percival = "";
+			}
+
+			if(payload.contains("#selectRole")){
+				String split1 = payload.split("myRoleNumber=")[1];
+				String roleNumStr = split1.split("&name=")[0];
+				String name = split1.split("&name=")[1];
+				System.out.println(roleNumStr);
+				System.out.println(name);
+				
+				switch (roleNumStr) {
+					case "1": //멀린
+						this.merlin = name;
+						break;
+					case "2": //퍼시벌
+						this.percival = name;
+						break;
+					case "3": //악의세력
+						this.minion = name;
+						break;
+					case "4": //어쌔신
+						this.assasin = name;
+						break;
+					case "5": //모르가나
+						this.morcana = name;
+						break;
+					case "6": //오베론
+						this.oberon = name;
+						break;
+					case "7": //모드레드
+						this.mordred = name;
+						break;
+					default:
+						break;
+				}
+
+			}
+			
+			if(payload.equals("#showResult")){
+				System.out.println("역할군 담기 시도");
+				mapData.put("result", "true");
+				mapData.put("merlin", this.merlin);
+				mapData.put("percival", this.percival);
+				mapData.put("minion", this.minion);
+				mapData.put("assasin", this.assasin);
+				mapData.put("morcana", this.morcana);
+				mapData.put("oberon", this.oberon);
+				mapData.put("mordred", this.mordred);
+				sendMessage(mapData);
+			}
+			else {
+				mapData.put("totalPlayerCtn", this.totalPlayerCtn.toString());
+				sendMessage(mapData);
+			}
+
+
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -59,6 +127,15 @@ public class SocketAvalonRoleSet extends TextWebSocketHandler {
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 		sessions.remove(session.getId());
 		this.totalPlayerCtn --;
+		if(totalPlayerCtn == 0) {
+			this.assasin = "";
+	    	this.merlin = "";
+			this.minion = "";
+			this.morcana = "";
+			this.mordred = "";
+			this.oberon = "";
+			this.percival = "";
+		}
 		super.afterConnectionClosed(session, status);
 		Map<String, String> mapData = new HashMap<>();
 		mapData.put("totalPlayerCtn", this.totalPlayerCtn.toString());
