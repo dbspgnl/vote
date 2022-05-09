252ed9dfabd6415b1daac3752b2c40871bcc8ad2
DESKTOP-1BI7C0V\user <swpark@yellotail.com>
2022-05-09 13:20:08 +0900

    + 화면 공유 중지/실행에 대한 동기화 + 화면 공유 시간에 대한 동기화

--- src/main/java/com/example/vote/Service/SocketScreenShare.java
+++ src/main/java/com/example/vote/Service/SocketScreenShare.java
@@ -21,23 +21,28 @@ public class SocketScreenShare extends TextWebSocketHandler {
 
 	HashMap<String, WebSocketSession> sessions = new HashMap<>();
 	HashMap<String, Object> mapData = new HashMap<String, Object>();
-	String messageString = "";
+	// Float shareTime = null;
 
 	// client에서 메시지가 서버로 전송댈때 실행되는 함수.
 	@Override
 	public void handleTextMessage(WebSocketSession session, TextMessage message) throws ParseException {
 		String payload = message.getPayload();
 		JSONObject json = jsonToObjectParser(payload); 
+		// System.out.println(json.get("video_timestamp"));
 		
 		try {
-			
+			System.out.println(json);
+			// shareTime = Float.parseFloat(json.get("video_timestamp").toString());
+			// System.out.println(shareTime);
+
 			HashMap<String, Object> hashMap = new HashMap<>();
-			hashMap.put("data", json);
-			hashMap.put("playing", false);
-			hashMap.put("streamable_url", "get_streamable_url(youtube_url)");
+			hashMap.put("video_timestamp", json.get("video_timestamp"));
+			hashMap.put("playing", json.get("playing"));
+			// hashMap.put("streamable_url", "get_streamable_url(youtube_url)");
 			hashMap.put("last_updated", new Timestamp(System.currentTimeMillis()).toString());
 			hashMap.put("global_time", 0);
 			hashMap.put("client_uid", sessions.keySet().toString());
+			// hashMap.put("video_shareTime", shareTime);
 					
 			JSONObject jsonObject = new JSONObject(hashMap);
 			// System.out.println(jsonObject.toJSONString());
